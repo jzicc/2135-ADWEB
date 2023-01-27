f57317f9806327b202ca44972538a19da33c59b0
jzicc <123297148+jzicc@users.noreply.github.com>
2023-01-27 21:07:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 2135-ADWEB
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cv-2135-jzc)
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { ReferencesComponent } from './references/references.component';
+
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, AboutComponent, SkillsComponent, EducationComponent, ReferencesComponent],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
